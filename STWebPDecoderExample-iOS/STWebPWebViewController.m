6bbe9b822ba2de3a277ac44f59901bd5a9e81e25
Scott Talbot <s@chikachow.org>
2013-09-24 23:37:03 +1000

    Update UIWebView example removing scheme prefix

--- STWebPDecoderExample-iOS/STWebPWebViewController.m
+++ STWebPDecoderExample-iOS/STWebPWebViewController.m
@@ -37,7 +37,7 @@
 //	double delayInSeconds = 2.0;
 //	dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
 //	dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-		[self.webView loadHTMLString:@"<img src=\"stwebp-https://www.gstatic.com/webp/gallery3/2_webp_ll.webp\">" baseURL:nil];
+		[self.webView loadHTMLString:@"<img src=\"https://www.gstatic.com/webp/gallery3/2_webp_ll.webp\">" baseURL:nil];
 //	});
 }
 
