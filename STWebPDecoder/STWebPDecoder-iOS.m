5fd9c2898c753922528b1229fd6e9f82a8df0114
Scott Talbot <s@chikachow.org>
2014-01-30 07:05:10 +1100

    Extract frames from animated images

--- STWebPDecoder/STWebPDecoder-iOS.m
+++ STWebPDecoder/STWebPDecoder-iOS.m
@@ -7,6 +7,7 @@
 #import "STWebPDecoder.h"
 
 #import "lib/libwebp/src/webp/decode.h"
+#import "lib/libwebp/src/webp/demux.h"
 
 
 NSString * const STWebPErrorDomain = @"STWebP";
@@ -24,46 +25,159 @@ static void STCGDataProviderReleaseDataCallbackFree(void * __unused info, const 
 }
 
 + (UIImage *)imageWithData:(NSData *)data scale:(CGFloat)scale error:(NSError * __autoreleasing *)error {
-	int w = 0, h = 0;
-	uint8_t *bitmapData = WebPDecodeBGRA(data.bytes, data.length, &w, &h);
-	if (!bitmapData) {
+	WebPData const webpData = (WebPData){ .bytes = data.bytes, .size = data.length };
+	WebPDemuxer *demux = WebPDemux(&webpData);
+	if (!demux) {
 		if (error) {
 			*error = [NSError errorWithDomain:STWebPErrorDomain code:STWebPDecodeFailure userInfo:nil];
 		}
 		return nil;
 	}
 
-	CGImageRef bitmap = NULL;
-	{
-		NSUInteger const bitsPerComponent = 8;
-		NSUInteger const bytesPerPixel = 4;
-		NSUInteger const bitsPerPixel = bitsPerComponent * bytesPerPixel;
-		NSUInteger const stride = (NSUInteger)w * bytesPerPixel;
 
-		CGBitmapInfo const bitmapInfo = kCGBitmapByteOrder32Little | kCGImageAlphaFirst;
+	uint32_t const w = WebPDemuxGetI(demux, WEBP_FF_CANVAS_WIDTH);
+	uint32_t const h = WebPDemuxGetI(demux, WEBP_FF_CANVAS_HEIGHT);
+	uint32_t const flags = WebPDemuxGetI(demux, WEBP_FF_FORMAT_FLAGS);
+	uint32_t const frameCount = WebPDemuxGetI(demux, WEBP_FF_FRAME_COUNT);
+	NSMutableArray *images = [[NSMutableArray alloc] initWithCapacity:frameCount];
 
-		CGColorSpaceRef drgb = CGColorSpaceCreateDeviceRGB();
-		if (drgb) {
-			CGDataProviderRef bitmapDataProvider = CGDataProviderCreateWithData(NULL, bitmapData, (size_t)(stride * h), STCGDataProviderReleaseDataCallbackFree);
+	NSTimeInterval duration = 0;
 
+	WebPIterator iter;
+	if (!WebPDemuxGetFrame(demux, 1, &iter)) {
+		WebPDemuxDelete(demux);
+		if (error) {
+			*error = [NSError errorWithDomain:STWebPErrorDomain code:STWebPDecodeFailure userInfo:nil];
+		}
+		return nil;
+	}
+
+	NSTimeInterval frameDurations[frameCount];
+	WebPDemuxGetFrame(demux, 1, &iter);
+	do {
+		frameDurations[iter.frame_num] = iter.duration / 1000.;
+	} while (WebPDemuxNextFrame(&iter));
+
+
+	CGColorSpaceRef colorspace = NULL;
+
+	WebPChunkIterator chunk_iter;
+	if (flags & ICCP_FLAG) {
+		WebPDemuxGetChunk(demux, "ICCP", 1, &chunk_iter);
+
+		CFDataRef const iccpData = CFDataCreateWithBytesNoCopy(NULL, chunk_iter.chunk.bytes, chunk_iter.chunk.size, kCFAllocatorNull);
+		colorspace = CGColorSpaceCreateWithICCProfile(iccpData);
+		CFRelease(iccpData);
+	}
+	WebPDemuxReleaseChunkIterator(&chunk_iter);
+	if (!colorspace) {
+		colorspace = CGColorSpaceCreateDeviceRGB();
+	}
+
+	CGBitmapInfo const bitmapInfo = kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedLast;
+	CGContextRef canvas = CGBitmapContextCreate(NULL, w, h, 8, 0, colorspace, bitmapInfo);
+	if (!canvas) {
+		if (error) {
+			*error = [NSError errorWithDomain:STWebPErrorDomain code:STWebPDecodeFailure userInfo:nil];
+		}
+		CGColorSpaceRelease(colorspace);
+		return nil;
+	}
+
+	WebPDemuxGetFrame(demux, 1, &iter);
+	do {
+		WebPData const frameData = iter.fragment;
+
+		int frameW = 0, frameH = 0;
+		uint8_t *bitmapData = WebPDecodeBGRA(frameData.bytes, frameData.size, &frameW, &frameH);
+		if (!bitmapData) {
+			if (error) {
+				*error = [NSError errorWithDomain:STWebPErrorDomain code:STWebPDecodeFailure userInfo:nil];
+			}
+			CGContextRelease(canvas);
+			CGColorSpaceRelease(colorspace);
+			return nil;
+		}
+
+		CGPoint const frameOrigin = (CGPoint){ .x = iter.x_offset, .y = (h - frameH) - iter.y_offset };
+		CGSize const frameSize = (CGSize){ .width = frameW, .height = frameH };
+		CGRect const frameRect = (CGRect){ .origin = frameOrigin, .size = frameSize };
+
+		CGImageRef bitmap = NULL;
+		{
+			NSUInteger const bitsPerComponent = 8;
+			NSUInteger const bytesPerPixel = 4;
+			NSUInteger const bitsPerPixel = bitsPerComponent * bytesPerPixel;
+			NSUInteger const stride = (NSUInteger)frameW * bytesPerPixel;
+
+			CGBitmapInfo const bitmapInfo = kCGBitmapByteOrder32Little | kCGImageAlphaFirst;
+
+			CGDataProviderRef bitmapDataProvider = CGDataProviderCreateWithData(NULL, bitmapData, (size_t)(stride * frameH), STCGDataProviderReleaseDataCallbackFree);
 			if (bitmapDataProvider) {
-				bitmap = CGImageCreate((size_t)w, (size_t)h, bitsPerComponent, bitsPerPixel, stride, drgb, bitmapInfo, bitmapDataProvider, NULL, YES, kCGRenderingIntentDefault);
+				bitmap = CGImageCreate((size_t)frameW, (size_t)frameH, bitsPerComponent, bitsPerPixel, stride, colorspace, bitmapInfo, bitmapDataProvider, NULL, YES, kCGRenderingIntentDefault);
 				CGDataProviderRelease(bitmapDataProvider);
 			}
-
-			CGColorSpaceRelease(drgb);
 		}
-	}
-	if (!bitmap) {
-		if (error) {
-			*error = [NSError errorWithDomain:STWebPErrorDomain code:STWebPDecodeFailure userInfo:nil];
+		if (!bitmap) {
+			if (error) {
+				*error = [NSError errorWithDomain:STWebPErrorDomain code:STWebPDecodeFailure userInfo:nil];
+			}
+			CGContextRelease(canvas);
+			CGColorSpaceRelease(colorspace);
+			return nil;
 		}
-		return nil;
+
+		switch (iter.blend_method) {
+			case WEBP_MUX_BLEND:
+				break;
+			case WEBP_MUX_NO_BLEND:
+				CGContextClearRect(canvas, frameRect);
+		}
+
+		CGContextDrawImage(canvas, frameRect, bitmap);
+		CFRelease(bitmap), bitmap = NULL;
+
+		CGImageRef canvasBitmap = CGBitmapContextCreateImage(canvas);
+		UIImage *image = [[UIImage alloc] initWithCGImage:canvasBitmap scale:scale orientation:UIImageOrientationUp];
+		CFRelease(canvasBitmap), canvasBitmap = NULL;
+
+		switch (iter.dispose_method) {
+			case WEBP_MUX_DISPOSE_NONE:
+				break;
+			case WEBP_MUX_DISPOSE_BACKGROUND:
+				CGContextClearRect(canvas, frameRect);
+				break;
+		}
+
+		[images addObject:image];
+		duration += iter.duration / 1000.;
+	} while (WebPDemuxNextFrame(&iter));
+
+	CGContextRelease(canvas), canvas = NULL;
+	CGColorSpaceRelease(colorspace), colorspace = NULL;
+
+	WebPDemuxReleaseIterator(&iter), iter = (WebPIterator){ };
+
+	// ... (Extract metadata).
+//	WebPChunkIterator chunk_iter;
+//	if (flags & ICCP_FLAG) WebPDemuxGetChunk(demux, "ICCP", 1, &chunk_iter);
+//	// ... (Consume the ICC profile in 'chunk_iter.chunk').
+//	WebPDemuxReleaseChunkIterator(&chunk_iter);
+//	if (flags & EXIF_FLAG) WebPDemuxGetChunk(demux, "EXIF", 1, &chunk_iter);
+//	// ... (Consume the EXIF metadata in 'chunk_iter.chunk').
+//	WebPDemuxReleaseChunkIterator(&chunk_iter);
+//	if (flags & XMP_FLAG) WebPDemuxGetChunk(demux, "XMP ", 1, &chunk_iter);
+//	// ... (Consume the XMP metadata in 'chunk_iter.chunk').
+//	WebPDemuxReleaseChunkIterator(&chunk_iter);
+
+
+	WebPDemuxDelete(demux), demux = NULL;
+
+	if (images.count == 1) {
+		return images.lastObject;
 	}
 
-	UIImage *image = [[UIImage alloc] initWithCGImage:bitmap scale:scale orientation:UIImageOrientationUp];
-	CFRelease(bitmap);
-
+	UIImage * const image = [UIImage animatedImageWithImages:images duration:duration];
 	return image;
 }
 
